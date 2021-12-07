0ec79cb49b8788df01a686799a464b5cd9838101
Alex Leakos <aleakos@gmail.com>
2021-12-07 13:39:45 -0700

    add populate db functions

--- ensf614project/src/model/Movie.java
+++ ensf614project/src/model/Movie.java
@@ -8,7 +8,6 @@ public class Movie {
 	private String name;
 	private String description;
 	private Date releaseDate;
-	private String releaseDateString;
 	
 	public Movie(int id, String name, Date releaseDate, String description, int runTime) {
 		super();
@@ -18,15 +17,6 @@ public class Movie {
 		this.releaseDate = releaseDate;
 		
 	}
-
-	public Movie(String name, String releaseDateString, String description, int runTime) {
-		super();
-		this.name = name;
-		this.description = description;
-		this.releaseDateString = releaseDateString;
-
-	}
-
 	public String getName() {
 		return name;
 	}
@@ -57,19 +47,10 @@ public class Movie {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	
+	
+	
+	
+	
 
-	public String getReleaseDateString() {
-		return releaseDateString;
-	}
-
-	@Override
-	public String toString() {
-		return "Movie{" +
-				"id=" + id +
-				", runTime=" + runTime +
-				", name='" + name + '\'' +
-				", description='" + description + '\'' +
-				", releaseDate=" + releaseDate +
-				'}';
-	}
 }
